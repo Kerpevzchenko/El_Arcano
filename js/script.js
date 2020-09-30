465a680c29927975beaceb1ec94371812d46179d
Leonardo <aaronleonidas@gmail.com>
2020-09-30 16:46:47 -0500

    Reacomodo de Bootstrap y agregado de footer y carusel

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    //alert("Esto es Jquery");
+
+    var menuNav = $(".topnav .a").width();
+    var NavElem = $(".topnav .a a");
+
+    var posElem = menuNav / NavElem.length;
+
+    NavElem.each(function(){
+        $(this).css("width",posElem);
+
+    });
+
+    //alert(menuNav);
+
+});
